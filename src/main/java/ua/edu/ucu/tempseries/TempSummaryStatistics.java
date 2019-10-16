94ceb38721d7ffdf0537a22207149ce7d5f7bc40
Danylo Sluzhynskyi <sluzhynskyi@gmail.com>
2019-10-16 23:57:28 +0300

    Replacment of methods2

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -5,36 +5,36 @@ public class TempSummaryStatistics {
     private double devTemp;
     private double minTemp;
     private double maxTemp;
-    
-    public void setAvgTemp(double avgTemp) {
-        this.avgTemp = avgTemp;
-    }
-
-    public void setDevTemp(double devTemp) {
-        this.devTemp = devTemp;
-    }
-
-    public void setMinTemp(double minTemp) {
-        this.minTemp = minTemp;
-    }
-
-    public void setMaxTemp(double maxTemp) {
-        this.maxTemp = maxTemp;
-    }
 
     public double getAvgTemp() {
         return avgTemp;
+    }
+
+    public void setAvgTemp(double avgTemp) {
+        this.avgTemp = avgTemp;
     }
 
     public double getDevTemp() {
         return devTemp;
     }
 
+    public void setDevTemp(double devTemp) {
+        this.devTemp = devTemp;
+    }
+
     public double getMinTemp() {
         return minTemp;
     }
 
+    public void setMinTemp(double minTemp) {
+        this.minTemp = minTemp;
+    }
+
     public double getMaxTemp() {
         return maxTemp;
+    }
+
+    public void setMaxTemp(double maxTemp) {
+        this.maxTemp = maxTemp;
     }
 }
